95e3e4d4bfd9da14b74d84ec95b60c324fdb2dd0
KathanKashiparekh <kathan.kparekh@gmail.com>
2017-08-27 23:43:22 +0530

    Add files via upload

---
+++ greyscale.py
@@ -0,0 +1,13 @@
+import numpy as np 
+from scipy import ndimage,misc
+from skimage.color import rgb2gray
+from skimage.color import rgb2hsv
+import tensorflow as tf 
+# s="/home/kathan/Desktop/release/data/train_images/100.png"
+# arr=ndimage.imread(s,mode="RGB")
+# grey=rgb2gray(arr)
+# grey_resized=misc.imresize(grey,(64,64))
+# misc.imsave('100Grey.png',grey_resized)
+
+
+print "Saved"
